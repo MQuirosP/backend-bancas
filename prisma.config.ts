9e97e0ae4d84ef6929de1d2da813b40726acbbd0
Mario Quirós <mquirosp78@gmail.com>
2025-10-16 14:25:53 -0600

    chore(prisma): routes fixed

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,6 @@
-// prisma.config.ts
+// src/prisma.config.ts
 import path from "node:path";
 import { defineConfig } from "prisma/config";
-
-// Si NO usas variables de entorno aquí, puedes omitir dotenv.
-// import "dotenv/config";
-
 export default defineConfig({
-  // Ruta a tu schema. Las rutas se resuelven relativas a ESTE archivo.
-  schema: path.join("prisma", "schema.prisma"),
+  schema: path.join("..", "prisma", "schema.prisma"),
 });
